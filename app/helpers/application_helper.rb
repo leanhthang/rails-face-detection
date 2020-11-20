87d5218147bda9b600ad7d244b8da734e9ef446c
Le Anh Thang <leanhthang70@gmail.com>
2020-11-20 12:01:04 +0700

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def sp_base_url
-    'http://localhost:3001'
+    'http://122.248.226.101:3001'
   end
   def backend_base_url
-    'http://localhost:3000'
+    'http://122.248.226.101:3000'
   end
 end
