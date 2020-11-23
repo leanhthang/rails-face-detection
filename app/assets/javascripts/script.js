a140261f82a55025bb8a6f8e0fbea65b313698f2
Le Anh Thang <leanhthang70@gmail.com>
2020-11-23 14:48:23 +0700

    update

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -59,11 +59,12 @@ function videoOnPlay() {
 }
 
 function loadFaceapi() {
+  video.play();
   Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+    // faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
   ]).then(startVideo);
 }
 function startVideo() {var video = $('#inputVideo')[0];
