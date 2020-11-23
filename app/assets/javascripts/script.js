8e26c8ddc59d2544a6409dd60c352e4554fadd7c
leanhthang <leanhthang70@gmail.com>
2020-11-24 00:35:06 +0700

    optimize faster

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -25,46 +25,59 @@ function videoOnPlay() {
   }
   loadFaceapi();
   loading(true);
+  let cameraAtion = null;
+  let detecttionIsActive = false;
   video.onplay = function() {
       const displaySize = { width: video.width, height: video.height }
       faceapi.matchDimensions(canvas, displaySize)
       console.log("asdas")
       setInterval(async () => {
-        // let image = await faceapi.fetchImage(avatar.src)
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-        .withFaceLandmarks().withFaceDescriptors()
-        // .withFaceExpressions()
-        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+          // let image = await faceapi.fetchImage(avatar.src)
+          const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+          .withFaceLandmarks().withFaceDescriptors()
+          // .withFaceExpressions()
+          const resizedDetections = faceapi.resizeResults(detections, displaySize)
 
-
-        const labeledFaceDescriptors = await loadLabeledImages()
-        const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.55)
-
-        const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-        results.forEach((result, i) => {
-          canvas.getContext('2d').clearRect(0, 0, video.width, video.height)
-          const box = resizedDetections[i].detection.box
-          if (result._distance <= 0.55) {
-            successDetector(result._distance);
-          } else {
-            resetDector();
+          if (detecttionIsActive == false) {
+            canvas.getContext('2d').clearRect(0, 0, video.width, video.height);
+            faceapi.draw.drawDetections(canvas, resizedDetections);
           }
-          const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-          drawBox.draw(canvas)
-        });
 
-        video.play();
-      }, 100)
+
+        clearTimeout(cameraAtion);
+        cameraAtion = setTimeout(async () => {
+
+          const labeledFaceDescriptors = await loadLabeledImages()
+          const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.55)
+
+          const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+
+
+            results.forEach((result, i) => {
+              canvas.getContext('2d').clearRect(0, 0, video.width, video.height)
+              detecttionIsActive = true;
+              const box = resizedDetections[i].detection.box
+              if (result._distance <= 0.55) {
+                successDetector(result._distance);
+              } else {
+                resetDector();
+              }
+              const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+              drawBox.draw(canvas)
+            });
+        }, 50);
+      }, 50)
   };
 }
 
 function loadFaceapi() {
+  var video = $('#inputVideo')[0];
   video.play();
   Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    // faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
   ]).then(startVideo);
 }
 function startVideo() {var video = $('#inputVideo')[0];
